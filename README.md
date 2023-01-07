cc3600cc1c7ed7c3f2eac171fceb687a1e11dfff
Commenter <35016761+Commenter25@users.noreply.github.com>
2023-01-07 14:51:59 -0600

    actual initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# YAPL
-Yet Another Preloader. The simplest JavaScript preloader I could think of. Probably too simple.
+<!-- markdownlint-disable MD033 -->
+# <img src="logo.svg" style="display:inline; height:1em; position:relative; top: 4px"> YAPL - Yet Another PreLoader
+The simplest JavaScript preloader I could think of. Probably too simple.
+
+Made for a private web game, because everything else I found looked complicated and scary, so I just made my own thing because I'm insane. Released as I felt others might find it useful. Besides the rest of that game, this is my first JavaScript project, so it probably sucks. This requires ES6, but it's so basic I think it could be refactored for ES3. I won't, because if you want that, I think you're probably more insane than me.
+
+Every file will be added to the DOM as a 1x1 pixel with 0.01 opacity, all within a div the script creates for you, which is fixed to the bottom left of the viewport. Everything is technically always visible and therefore the browser should always load and cache it, but it should be imperceivable to any human. If anyone spots it, say it's a reference to CRAM dots, because it is ;D
+
+---
+
+## Basic usage
+All you have to do is pass two parameters to `YAPLload()`.
+
+The first is the folder which contains every link you want to pass to. This could be `""` to just use the current page, `"/"` for root, or something like `"/assets"` to avoid restating that folder over and over.
+
+The second is an array of file paths, relative to the first parameter. It can look something like the following code block:
+
+```javascript
+const assets = [
+  "img/cool-img.webp",
+  "snd/wacky-sound.ogg"
+];
+```
+
+These parameters will be combined to make a full path, like `"/assets/img/cool-img.webp"` and `"/assets/snd/wacky-sound.ogg"`.
+
+If you use this ANYWHERE besides your own machine, ***KEEP YOUR LIST AS SHORT AS POSSIBLE!*** It will request all files in parallel at once, and HTTP requests add up fast. Spritesheets recommended!
+
+---
+
+## Using functions
+
+You can optionally pass 3 functions in order: one for after all files are parsed, one for every time a file's parsed, and one if YAPL completely fails. Two variables are also accessible, `YAPLloaded` and `YAPLfailed`. You can add these together to get the total number of files processed.
+
+I personally loaded YAPL with defer, then contained my assets and functions in an inline `<script>` wrapped in a `DOMContentLoaded` listener. For each file, it updated a loading indicator in a disabled button. Then when done, it'd add a new `<script>` tag to `<head>`, loading a script contained all my actual code, which I only wanted to run once everything was loaded. In my new script, I activated the button to allow starting the game. Here's an code block showing that usage:
+
+```javascript
+document.addEventListener("DOMContentLoaded", ()=> {
+  const folder = "/assets";
+  const assets = [
+    "img/cool-img.webp",
+    "snd/wacky-sound.ogg"
+  ];
+
+  function done() {
+    const tag = document.createElement('script');
+    tag.src = "assets/js/main.js";
+    document.head.appendChild(tag);
+  }
+
+  function incr() {
+    document.getElementById("loadbutton").innerHTML = `Loading... ${YAPLloaded + YAPLfailed + "/" + assets.length}`;
+  }
+
+  function fail() {
+    document.getElementById("loadbutton").innerHTML = "Something has gone horribly wrong!";
+  }
+
+  YAPLload(folder, assets, done, incr, fail);
+});
+```
---
+++ yapl.js
@@ -0,0 +1,103 @@
+/*! @license Yet Another PreLoader v1.0.0 | Copyright (c) 2023 Commenter25 | MIT License  */
+/* @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT License */
+/* jshint esversion: 6, browser: true, devel: true, strict: global */ "use strict";
+
+let YAPLloaded = 0, YAPLfailed = 0; 
+
+/**
+ * @param {string} filename - String ending with a file extension, like "cool-img.webp" or "wacky-sound.ogg"
+ * @returns {string} Returns the file extension at the end of a string.
+ * @author Tomalak // https://stackoverflow.com/a/680982
+ */
+function getExtension(string) {
+	return /(?:\.([^.]+))?$/.exec(string)[1];
+}
+
+/**
+ * Adds a series of files to the DOM so the browser caches them
+ * @param {string} location - Path containing all files, relative to the current page
+ * @param {array} files - Array of strings with file paths relative to folder param - If using in ANY non-localhost context, KEEP AS SHORT AS POSSIBLE!!!!
+ * @param {function} whenDone - Optional - Runs when all files have been parsed
+ * @param {function} whenIncr - Optional - Runs every time a file is parsed, useful for progress indication
+ * @param {function} onFail - Optional - For the event of complete failure - Using the phrase "Something has gone horribly wrong!" somewhere is recommended
+ * @returns {Promise} Promise resolving when all files have been parsed
+ * @throws Returns false and logs an exception to console if not given an array
+ */ 
+function YAPLload(location, files, whenDone = false, whenIncr = false, onFail = false) {
+try { return new Promise( (resolve)=> {
+	if (files === undefined || !Array.isArray(files)) {
+		throw "Was not given an array in the files parameter!";
+	}
+
+	// always have folder end in a slash, unless blank, so it makes a file path
+	let folder = location;
+	if (folder !== "" && folder.slice(-1) !== "/") folder = folder + '/';
+
+	const tagLoader = document.createElement("div");
+	tagLoader.id = "yet-another-preloader";
+	tagLoader.style = "position: fixed; top: 99.9vh; left: 0; display: flex; opacity: 0.01";
+	document.body.prepend(tagLoader);
+
+	const increment = worked => {
+		worked ? YAPLloaded++ : YAPLfailed++; // jshint ignore:line
+		if (whenIncr) whenIncr();
+		
+		if (YAPLloaded + YAPLfailed === files.length) {
+			console.log(`YAPL: Execution complete! ${YAPLloaded} successful, ${YAPLfailed} failed`);
+			if (whenDone) whenDone(); 
+			resolve();
+		}
+	};
+
+	const loadTag =(tag, file)=> {
+		function good() {
+			// console.log(`YAPL: ${file} loaded successfully!`);
+			tagLoader.appendChild(tag);
+			increment(true);
+		}
+		function bad() {
+			console.error(`YAPL: ${file} couldn't load!`);
+			increment(false);
+		}
+	
+		tag.src = file;
+		tag.controls = true;
+		tag.style = "width: 1px; height: 1px";
+		tag.addEventListener("load", good);
+		tag.addEventListener("canplay", good);
+		tag.addEventListener("error", bad);
+		return true; // returns loadFile as true
+	};
+
+	const loadFile =(file, type)=> {
+		let tag;
+		switch (type) {
+			// images
+			case "webp": case "png": case "gif":
+			case "jpg": case "jpeg": case "avif":
+				tag = new Image();
+				return loadTag(tag, file);
+			// sounds
+			case "ogg": case "mp3": case "wav":
+				tag = new Audio();
+				return loadTag(tag, file);
+			// videos
+			case "webm": case "mp4":
+				tag = document.createElement("video");
+				return loadTag(tag, file);
+			default:
+				console.error(`YAPL: ${file} is unrecognized file type ${type}!`);
+				return false;
+		}
+	};
+
+	for (let i of files) loadFile(folder + i, getExtension(i));
+	
+	console.log(`YAPL: Attempting to load all files...`);
+});} catch (e) {
+	console.error("YAPL: Execution failure! " + e);
+	if (onFail) onFail(); 
+	return false;
+}}
+
+/* @license-end */
